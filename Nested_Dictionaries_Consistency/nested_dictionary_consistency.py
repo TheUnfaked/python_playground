f274a3c40851cb15acd3be1f474424a9bdb4465f
TheUnfaked <wacker.thomas@live.de>
2020-10-25 02:51:50 +0200

    Generalized a bit more

--- Nested_Dictionaries_Consistency/nested_dictionary_consistency.py
+++ Nested_Dictionaries_Consistency/nested_dictionary_consistency.py
@@ -1,6 +1,8 @@
 def nd_consistency(d):
+	counter = 0
 	temp = []
 	for i in d.values():
+		counter += 1
 		if temp == []:
 			temp = [i for i in i]
 		else:
@@ -12,7 +14,7 @@ def nd_consistency(d):
 		temp += [v for v in v]
 	temp_d = {i:temp.count(i) for i in temp}
 	max_val = max(temp_d.values())
-	if all(value == max_val for value in temp_d.values()):
+	if all(value == max(max_val, counter) for value in temp_d.values()):
 		return True
 	else:
 		return False
