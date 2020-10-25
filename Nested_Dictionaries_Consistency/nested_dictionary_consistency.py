16b58953810ff6cbcd1b99d5474c0a1605e8672d
TheUnfaked <wacker.thomas@live.de>
2020-10-25 02:27:29 +0200

    Generalized a bit more

--- Nested_Dictionaries_Consistency/nested_dictionary_consistency.py
+++ Nested_Dictionaries_Consistency/nested_dictionary_consistency.py
@@ -1,5 +1,13 @@
 def nd_consistency(d):
 	temp = []
+	for i in d.values():
+		if temp == []:
+			temp = [i for i in i]
+		else:
+			for j in i:
+				if j not in temp:
+					return False
+	temp = []
 	for k,v in d.items():
 		temp += [v for v in v]
 	temp_d = {i:temp.count(i) for i in temp}
